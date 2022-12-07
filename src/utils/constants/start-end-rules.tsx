8b3128db8668fbbafd492a1cbb413e0a08474cf1
nikthegeek1 <ntheodoropoulos@outlook.com>
2022-12-07 21:40:00 +0200

    start-end-rules bug

--- src/utils/constants/start-end-rules.tsx
+++ src/utils/constants/start-end-rules.tsx
@@ -27,12 +27,12 @@ export const get_OPTIONS_BY_WHOAMI_AND_NAME = (
 ) => {
   const options = rule === "start" ? START_OPTIONS_BY_WHOAMI_AND_NAME : END_OPTIONS_BY_WHOAMI_AND_NAME;
   try {
-    return options[whoAmI][addonInputName];
+    return options[whoAmI][addonInputName] || [];
   } catch (e1) {
     // trying removing the last char of the whoAmI in case it's a dubplicate addon and so last char == number
     const l = whoAmI.length;
     try {
-      return options[whoAmI.slice(0, l-1)][addonInputName];
+      return options[whoAmI.slice(0, l-1)][addonInputName] || [];
     } catch (e) {
       console.log(
         `No specific ${rule} options for`,
